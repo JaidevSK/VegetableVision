c77d5c7357be432c7333e340072b14e218843d06
JaidevSK <134712750+JaidevSK@users.noreply.github.com>
2024-01-27 17:41:42 +0530

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import sys
+
+# Get the absolute path of the current file
+file_path = Path(__file__).resolve()
+
+# Get the parent directory of the current file
+root_path = file_path.parent
+
+# Add the root path to the sys.path list if it is not already there
+if root_path not in sys.path:
+    sys.path.append(str(root_path))
+
+# Get the relative path of the root directory with respect to the current working directory
+ROOT = root_path.relative_to(Path.cwd())
+
+# Sources
+IMAGE = 'Image'
+
+SOURCES_LIST = [IMAGE]
+
+# ML Model config
+MODEL_DIR = ROOT / 'weights'
+DETECTION_MODEL = MODEL_DIR / 'vege.pt'
+# In case of your custome model comment out the line above and
+# Place your custom model pt file name at the line below 
+# DETECTION_MODEL = MODEL_DIR / 'my_detection_model.pt'
+
+SEGMENTATION_MODEL = MODEL_DIR / 'yolov8n-seg.pt'
+
